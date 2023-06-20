3347de63043934e2426c9e9dae0ba9e00573c114
JoseAdrianRodriguezGonzalez <88957069+JoseAdrianRodriguezGonzalez@users.noreply.github.com>
2023-06-20 13:36:58 -0600

    Add files via upload

---
+++ Ventana.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ventana;
+import igu.panel;
+/**
+ *
+ * @author USUARIO
+ */
+public class Ventana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        panel panta= new panel();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+    }
+    
+}
